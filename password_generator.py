b2dea817dcb75633ba0a2d954b34705492367294
Alexander <shurpo.alexander@gmail.com>
2021-02-03 22:16:08 +0300

    Add function pass_generator plus refactoring

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,9 @@
 import random
 import string
 import clipboard
-random = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(8)])
-clipboard.copy(random)
+
+
+def pass_generator():
+    random_pass = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(8)])
+    copy_in_buffer = clipboard.copy(random_pass)
+    return copy_in_buffer
