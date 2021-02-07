75c5196150d50e005af9263176ac0e9ecb8a2de3
TurtleOld <shurpo.alexander@gmail.com>
2021-02-07 17:42:12 +0300

    Добавлены параметры окна и различных элекментов

---
+++ parameters_generator.py
@@ -0,0 +1,30 @@
+# main parameters
+title_window = "Generator passwords"
+size_windows = "500x300"
+
+# label
+length_password_text = "Length password:"
+checkbox_password_letters_text = "Use letters?"
+
+# position label
+length_password_x = "20"
+length_password_y = "15"
+
+checkbox_password_label_letters_x = "20"
+checkbox_password_label_letters_y = "40"
+
+
+# position blocks
+entry_password_length_x = "120"
+entry_password_length_y = "17"
+entry_password_length_width = "30"
+
+text_result_x = "170"
+text_result_y = "150"
+text_result_width = "120"
+text_result_height = "20"
+
+# text button and position
+text_button = "Generator password!"
+position_button_x = "200"
+position_button_y = "20"
