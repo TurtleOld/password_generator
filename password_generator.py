7154d46b7797513d6cdde333be5d4e51c1294432
TurtleOld <shurpo.alexander@gmail.com>
2021-02-07 17:42:50 +0300

    Файл удалён

--- password_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-import clipboard
-
-
-def pass_generator():
-    random_pass = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(8)])
-    copy_in_buffer = clipboard.copy(random_pass)
-    return copy_in_buffer
